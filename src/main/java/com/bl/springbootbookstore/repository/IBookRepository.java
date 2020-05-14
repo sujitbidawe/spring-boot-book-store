f53832c78cae063a0675bf46c4a093232c8124be
sujitbidawe <bidawesujit@gmail.com>
2020-05-14 20:55:39 +0530

    added: annotation for repository

--- src/main/java/com/bl/springbootbookstore/repository/IBookRepository.java
+++ src/main/java/com/bl/springbootbookstore/repository/IBookRepository.java
@@ -2,6 +2,8 @@ package com.bl.springbootbookstore.repository;
 
 import com.bl.springbootbookstore.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IBookRepository extends JpaRepository<Book, Integer> {
 }
