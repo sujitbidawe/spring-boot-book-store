be284d012a96434e1fe3b0ac25cca10e0471697a
sujitbidawe <bidawesujit@gmail.com>
2020-05-14 20:53:59 +0530

    refactored: code to remove lombok. added: constructors

--- src/main/java/com/bl/springbootbookstore/model/Book.java
+++ src/main/java/com/bl/springbootbookstore/model/Book.java
@@ -1,13 +1,8 @@
 package com.bl.springbootbookstore.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.persistence.*;
 @Entity
 @Table(name = Book.TABLE_NAME)
-@Getter
-@Setter
 public class Book {
     public static final String TABLE_NAME= "books";
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
@@ -19,12 +14,45 @@ public class Book {
     String description;
     int bookCount;
 
-//    public Book(int id, String bookTitle, String imageUrl, double price, String description, int bookCount) {
-//        this.id = id;
-//        this.bookTitle = bookTitle;
-//        this.imageUrl = imageUrl;
-//        this.price = price;
-//        this.description = description;
-//        this.bookCount = bookCount;
-//    }
+    public Book() {
+    }
+
+    public Book(int id, String authorName, String bookTitle, String image, double price, String description, int bookCount) {
+        this.id = id;
+        this.authorName = authorName;
+        this.bookTitle = bookTitle;
+        this.image = image;
+        this.price = price;
+        this.description = description;
+        this.bookCount = bookCount;
+    }
+
+    public Book(int id, String authorName, String bookTitle, String image, double price, String description) {
+        this.id = id;
+        this.authorName = authorName;
+        this.bookTitle = bookTitle;
+        this.image = image;
+        this.price = price;
+        this.description = description;
+    }
+
+    public Book(int id, String bookTitle, double price, int bookCount) {
+        this.id = id;
+        this.bookTitle = bookTitle;
+        this.price = price;
+        this.bookCount = bookCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", authorName='" + authorName + '\'' +
+                ", bookTitle='" + bookTitle + '\'' +
+                ", image='" + image + '\'' +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                ", bookCount=" + bookCount +
+                '}';
+    }
 }
